05f96813a925cc1686faec2f1ac9d88d96ddb6af
KNToad <tendmitriy02052016@gmail.com>
2025-03-23 18:50:47 +0300

    init commit

---
+++ src/main/java/ru/demo/hogwarts_dbhw/HogwartsDbhwApplication.java
@@ -0,0 +1,13 @@
+package ru.demo.hogwarts_dbhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsDbhwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsDbhwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/demo/hogwarts_dbhw/HogwartsDbhwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.demo.hogwarts_dbhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsDbhwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
