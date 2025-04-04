7a8cc09a92f680d47d8fd74268f40483804bf275
shawt <shawtyhead@mail.ru>
2025-04-04 17:49:07 +0300

    init commit

--- src/main/java/ru/demo/hogwarts_dbhw/controller/StudentController.java
+++ src/main/java/ru/demo/hogwarts_dbhw/controller/StudentController.java
@@ -1,4 +1,64 @@
 package ru.demo.hogwarts_dbhw.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.demo.hogwarts_dbhw.model.Student;
+import ru.demo.hogwarts_dbhw.service.StudentService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("student")
 public class StudentController {
+
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public Student add(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Student> findFaculty(@PathVariable long id) {
+        Student student = studentService.findStudent(id);
+        if (student == null) {
+            ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
+    }
+
+    @PutMapping
+    public ResponseEntity<Student> editStudent(@RequestBody Student student) {
+        Student foundStudent = studentService.editStudent(student);
+        if (foundStudent == null) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(foundStudent);
+    }
+
+    @DeleteMapping("{id}}")
+    public ResponseEntity deleteStudent(@PathVariable long id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Student>> getAllStudents() {
+        return ResponseEntity.ok(studentService.getAllStudents());
+    }
+
+    @GetMapping("{age}")
+    public ResponseEntity<Collection<Student>> getStudentsWithSameAge(@PathVariable int age) {
+        Collection<Student> students = studentService.getStudentsByAge(age);
+        if (students == null) {
+            ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(students);
+    }
 }
--- src/main/java/ru/demo/hogwarts_dbhw/repository/StudentRepository.java
+++ src/main/java/ru/demo/hogwarts_dbhw/repository/StudentRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.demo.hogwarts_dbhw.model.Student;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByAge(int age);
+
+    List<Student> findByAgeBetween(int min, int max);
 }
